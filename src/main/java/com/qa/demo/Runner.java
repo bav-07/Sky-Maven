2484af7b75d5c2b4f2234635c781a27c33ed8174
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-07-26 11:02:20 +0100

    initialised maven project

---
+++ src/main/java/com/qa/demo/Runner.java
@@ -0,0 +1,12 @@
+package com.qa.demo;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        System.out.println("MAVEN TEST");
+        System.out.println(Driver.drive("Nissan Micra"));
+
+    }
+
+}
